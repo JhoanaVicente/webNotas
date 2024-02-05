9ee795ca8ee51e50ffb83f9741510b493fbff14d
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2024-02-05 22:32:55 +0100

    Delete all and I turn do again

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,6 @@
 	let nombre= 'Jhoana';
 
 	import Boton from "./components/Boton.svelte";
-  import QuestionA from "./components/QuestionA.svelte";
-	import TestA from "./components/TestA.svelte";
-
 </script>
 
 <main>
@@ -13,13 +10,11 @@
 
 	<p>Hola, {nombre}!</p>
 	<Boton />
-	<TestA />
-	<QuestionA />
 </main>
 
 <style>
-	/*h1 {
+	h2 {
 		color: green;
-	}*/
+	}
 
 </style>
